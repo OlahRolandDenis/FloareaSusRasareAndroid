25a2e33908ca37eeb69e24342209cafe7a34b499
Katty Ptk <kattyptk@Kattys-MacBook-Pro.local>
2023-04-11 15:49:36 +0300

    removed stuff from settings

--- app/src/main/java/com/example/testwifi3/LedControlTask.java
+++
@@ -1,4 +0,0 @@
-package com.example.testwifi3;
-
-public interface LedControlTask {
-}
--- app/src/main/java/com/example/testwifi3/SettingsActivity.java
+++ app/src/main/java/com/example/testwifi3/SettingsActivity.java
@@ -20,82 +20,10 @@ import java.util.List;
 import java.util.Set;
 
 public class SettingsActivity extends AppCompatActivity {
-
-    private MainActivity mainActivity;
-    private UserSettings settings;
-
-    Button addNewIpBtn, saveBtn;
-    EditText ipAddressInput;
-
-    int counter = 0;
-    List<String> items = new LinkedList<>();
-
-    IPsAdapter adapter;
-
-    SharedPreferences sharedPreferences;
-    SharedPreferences.Editor editor;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        sharedPreferences = getSharedPreferences(UserSettings.PREFERENCES, MODE_PRIVATE);
-        editor = sharedPreferences.edit();
-
-
-        settings = ( UserSettings ) getApplication();
-        mainActivity = new MainActivity();
-
-        saveBtn = ( Button ) findViewById(R.id.save1Btn);
-        addNewIpBtn = ( Button ) findViewById(R.id.addNewIpBtn);
-        ipAddressInput = (EditText) findViewById(R.id.ipAddressInput);
-
-        Set<String> sharedPreferencesSet = sharedPreferences.getStringSet("ALL_IP_ADDRESSES", null);
-
-        if ( sharedPreferencesSet != null )
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
-                items.addAll(sharedPreferencesSet);
-
-        RecyclerView recyclerView = findViewById(R.id.ipsRecyclerView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this ));
-        adapter = new IPsAdapter(items);
-        recyclerView.setAdapter(adapter);
-
-        initIPAddressListener();
-
-        saveBtn.setOnClickListener( v -> {
-            saveItemsToSharedPreferences();
-            navigateToMainActivity();
-        });
-    }
-
-    private void initIPAddressListener() {
-
-        addNewIpBtn.setOnClickListener( v -> {
-            settings.setIPAddress(ipAddressInput.getText().toString());
-
-            editor.putString(settings.getIPAddress(), settings.getIPAddress());
-            editor.apply();
-
-            items.add(ipAddressInput.getText().toString());
-            counter += 1;
-            adapter.notifyItemInserted(items.size() - 1);
-        });
-
-    }
-
-    private void saveItemsToSharedPreferences() {
-        Set<String> set = new HashSet<String>();
-        set.addAll(items);
-
-        editor.putStringSet("ALL_IP_ADDRESSES", set).apply();
-    }
-
-    public void navigateToMainActivity() {
-        Log.d("NAVIGATE", "click to navigate to settings :D");
-
-        Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
-        startActivity(intent);
     }
 }
